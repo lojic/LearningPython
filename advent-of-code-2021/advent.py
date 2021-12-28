68f8d675cbfe42ae29fc8b81e0e7ef3fab7640f8
Brian Adkins <brian@lojic.com>
2021-12-28 12:04:20 -0500

    Day 01

---
+++ advent-of-code-2021/advent.py
@@ -0,0 +1,4 @@
+import unittest
+
+def file_to_lines(fname):
+    return open(fname).read().splitlines()
---
+++ advent-of-code-2021/day01.py
@@ -0,0 +1,34 @@
+from advent import *
+
+input = [ int(x) for x in file_to_lines('day01.txt') ]
+
+def count_increases(lst):
+    """Return a count of the number of times a number in the list is
+    greater than the preceding number."""
+    return len([ y for (x,y) in zip(lst, lst[1:]) if y > x])
+
+def windows(lst):
+    """Return a list of 3-element sliding windows from a list"""
+    return list(zip(lst, lst[1:], lst[2:]))
+
+def part1():
+    return count_increases(input)
+
+def part2():
+    return count_increases(list(map(sum, windows(input))))
+
+class TestDay01(unittest.TestCase):
+    def test_count_increase(self):
+        self.assertEqual(count_increases([3,2,1]), 0)
+        self.assertEqual(count_increases([3,2,3,1,2]), 2)
+
+    def test_windows(self):
+        self.assertEqual(windows([1,2,3,4,5]), [(1,2,3),(2,3,4),(3,4,5)])
+
+    def test_part1(self):
+        self.assertEqual(part1(), 1616)
+        
+    def test_part2(self):
+        self.assertEqual(part2(), 1645)
+
+unittest.main()
