4b7328e8dcf18eab7d4ec5abfc152771b1778f5b
Brian Adkins <brian@lojic.com>
2024-12-01 16:31:27 -0500

    Use tuple unpacking in part 1

--- advent-of-code-2024/day01.py
+++ advent-of-code-2024/day01.py
@@ -3,7 +3,7 @@ from advent import ints, parse, Counter
 left, right = zip(*parse(1, ints))
 
 # Part 1
-assert sum([ abs(x[0] - x[1]) for x in zip(sorted(left), sorted(right)) ]) == 1189304
+assert sum([ abs(l - r) for l, r in zip(sorted(left), sorted(right)) ]) == 1189304
 
 # Part 2
 c = Counter(right)
