f2a5e4fa5f26db17f0debffb49ab5a342d91167d
Brian Adkins <brian@lojic.com>
2024-12-01 17:05:01 -0500

    decouple assert from solutions

--- advent-of-code-2024/day01.py
+++ advent-of-code-2024/day01.py
@@ -2,9 +2,14 @@ from advent import ints, parse, Counter
 
 left, right = zip(*parse(1, ints))
 
-# Part 1
-assert sum([ abs(l - r) for l, r in zip(sorted(left), sorted(right)) ]) == 1189304
+def part1():
+    return sum([ abs(l - r) for l, r in zip(sorted(left), sorted(right)) ])
 
-# Part 2
-c = Counter(right)
-assert sum([ n * c[n] for n in left ]) == 24349736
+def part2():
+    c = Counter(right)
+    return sum([ n * c[n] for n in left ])
+
+# ---------------------------------------------------------------------------------------------
+
+assert part1() == 1189304
+assert part2() == 24349736
