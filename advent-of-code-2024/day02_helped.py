ccc6d7a5734395a22ae93e9551d21fa72039df55
Brian Adkins <brian@lojic.com>
2024-12-02 11:27:59 -0500

    Add a version using input from others

---
+++ advent-of-code-2024/day02_helped.py
@@ -0,0 +1,23 @@
+from advent import parse, ints, combinations
+
+input = parse(2, ints)
+
+def is_safe(t):
+    deltas = [ b - a for a, b in zip(t, t[1:]) ]
+
+    return all(delta in range(1,4) for delta in deltas) or \
+        all(delta in range(-3, 0) for delta in deltas)
+
+def is_dampened_safe(t):
+    return any(is_safe(t) for t in combinations(t, len(t) - 1))
+
+def part1():
+    return sum([ 1 for t in input if is_safe(t) ])
+
+def part2():
+    return sum([ 1 for t in input if is_safe(t) or is_dampened_safe(t) ])
+
+# ---------------------------------------------------------------------------------------------
+
+assert part1() == 510
+assert part2() == 553
