108cedcb54be6138551405a50c09dca297856e00
Brian Adkins <brian@lojic.com>
2024-12-03 09:54:17 -0500

    AoC 2024 Day 3

---
+++ advent-of-code-2024/day03.py
@@ -0,0 +1,22 @@
+from advent import parse, re
+
+input = "".join(parse(3))
+
+def solve(ignore_commands=False):
+    enabled = True
+    sum     = 0
+
+    for match in re.finditer(r"mul\((\d{1,3}),(\d{1,3})\)|don't\(\)|do\(\)", input):
+        if match.group() == "do()":
+            enabled = True
+        elif match.group() == "don't()":
+            enabled = False
+        elif enabled or ignore_commands:
+            sum += (int(match.group(1)) * int(match.group(2)))
+
+    return sum
+
+# ---------------------------------------------------------------------------------------------
+
+assert solve(True) == 174960292
+assert solve()     == 56275602
