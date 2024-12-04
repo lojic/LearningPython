a98932b745d87f5d523ad3a70758035583f17c8f
Brian Adkins <brian@lojic.com>
2024-12-04 11:43:38 -0500

    use lambda

--- advent-of-code-2024/day04.py
+++ advent-of-code-2024/day04.py
@@ -10,17 +10,13 @@ def get(c):
     return input[y][x] if 0 <= x < width and 0 <= y < height else None
 
 def part1():
-    def check(c, dir):
-        return ['X','M','A','S'] == [ get(c+n*dir) for n in range(4) ]
+    check = lambda c, dir: ['X','M','A','S'] == [ get(c+n*dir) for n in range(4) ]
 
     return sum(check(complex(x, y), dir) for dir in dirs for x in range(width) for y in range(height))
 
 def part2():
-    def check(c, dir):
-        return ['M','A','S'] == [ get(c+n*dir) for n in (-1, 0, 1) ]
-
-    def is_x(c):
-        return (check(c, ne) or check(c, sw)) and (check(c, nw) or check(c, se))
+    check = lambda c, dir: ['M','A','S'] == [ get(c+n*dir) for n in (-1, 0, 1) ]
+    is_x  = lambda c: (check(c, ne) or check(c, sw)) and (check(c, nw) or check(c, se))
 
     return sum(is_x(complex(x, y)) for x in range(width) for y in range(height))
 
