288fdbdd74f9472891dfb14907e3b6e72e3c060d
Brian Adkins <brian@lojic.com>
2024-12-04 22:11:58 -0500

    use set comp and intersection

--- advent-of-code-2024/day04_with_lib.py
+++ advent-of-code-2024/day04_with_lib.py
@@ -7,9 +7,9 @@ def part1():
     return len(grid_word_search(input, "XMAS"))
 
 def part2():
-    diag1 = { (x, y) : True for (x, y, d) in grid_word_search(input, "MAS", (-1, 0, 1), (ne, sw)) }
-    diag2 = { (x, y) : True for (x, y, d) in grid_word_search(input, "MAS", (-1, 0, 1), (se, nw)) }
-    return sum(diag2.get(key, False) for key in diag1.keys())
+    diag1 = { (x, y) for (x, y, d) in grid_word_search(input, "MAS", (-1, 0, 1), (ne, sw)) }
+    diag2 = { (x, y) for (x, y, d) in grid_word_search(input, "MAS", (-1, 0, 1), (se, nw)) }
+    return len(diag1 & diag2)
 
 # ---------------------------------------------------------------------------------------------
 
