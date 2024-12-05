705d2ca826ddf95de3072e48f9ac0381eb92c3e5
Brian Adkins <brian@lojic.com>
2024-12-05 11:26:06 -0500

    Removed a superfluous lambda

--- advent-of-code-2024/day05.py
+++ advent-of-code-2024/day05.py
@@ -7,8 +7,7 @@ for p1, p2 in rules: order_dict[p1].add(p2)
 is_ordered     = lambda t: list(t) == sort_pages(t)
 middle         = lambda t: t[len(t) // 2]
 sort_pages     = lambda t: sorted(t, key=cmp_to_key(lambda a, b: -1 if b in order_dict[a] else 1))
-fix_updates    = lambda u: [ sort_pages(t) for t in u if not is_ordered(t) ]
 
 assert sum(middle(t) for t in updates if is_ordered(t)) == 6949 # Part 1
 
-assert sum(middle(t) for t in fix_updates(updates)) == 4145 # Part 2
+assert sum(middle(sort_pages(t)) for t in updates if not is_ordered(t)) == 4145 # Part 2
