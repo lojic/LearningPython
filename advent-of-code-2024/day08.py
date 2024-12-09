ef1542a4ed9dc74f8d7c58fca86136745b5cd35c
Brian Adkins <brian@lojic.com>
2024-12-08 20:04:17 -0500

    Steal Todd's diff 3rd param idea

--- advent-of-code-2024/day08.py
+++ advent-of-code-2024/day08.py
@@ -7,16 +7,16 @@ valid_index   = lambda i: 0 <= i.real < width and 0 <= i.imag < height
 solve         = lambda get_nodes: len({ n
                                         for _, s in antennas.items()
                                         for i, j in combinations(s, 2)
-                                        for n in get_nodes(i, j) })
+                                        for n in get_nodes(i, j, i - j) })
 
 for k, v in grid_to_hash(lines, elem_filter=lambda c: c != '.').items():
     antennas[v].add(k)
 
-def part1(i, j):
-    return [ n for n in [ i + (i - j), j + (j - i) ] if valid_index(n) ]
+def part1(i, j, diff):
+    return [ n for n in [ i + diff, j - diff ] if valid_index(n) ]
 
-def part2(i, j):
-    step = (i-j) / gcd(int((i-j).real), int((i-j).imag))
+def part2(i, j, diff):
+    step = diff / gcd(int(diff.real), int(diff.imag))
     return { n * step + i for n in range(-width, width) if valid_index(n * step + i) }
 
 # ---------------------------------------------------------------------------------------------
