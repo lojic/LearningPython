3990d82255195fd555b341f76254492a493e2884
Brian Adkins <brian@lojic.com>
2024-12-10 14:00:08 -0500

    no need for width, height

--- advent-of-code-2024/day10.py
+++ advent-of-code-2024/day10.py
@@ -1,8 +1,8 @@
 from advent import parse, digits, compose, defaultdict, grid_to_hash
 
-input         = parse(10, digits)
-grid          = defaultdict(lambda: None, grid_to_hash(input))
-width, height = len(input[0]), len(input)
+input = parse(10, digits)
+grid  = defaultdict(lambda: None, grid_to_hash(input))
+solve = lambda score: sum(score(dfs(head, -1, [])) for head in [ pos for pos, level in grid.items() if level == 0 ])
 
 def dfs(head, prev, result):
     level = grid[head]
@@ -15,10 +15,6 @@ def dfs(head, prev, result):
         for dir in (1, 1j, -1, -1j):
             dfs(head + dir, level, result)
         return result
-
-def solve(score):
-    heads = [ pos for x in range(width) for y in range(height) if grid[(pos := complex(x, y))] == 0 ]
-    return sum(score(dfs(head, -1, [])) for head in heads)
 
 # ---------------------------------------------------------------------------------------------
 
