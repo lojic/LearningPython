34177ca466f794329f36fb7eb96a6f4a9f5e9857
Brian Adkins <brian@lojic.com>
2024-12-10 14:20:25 -0500

    shorten comprehension

--- advent-of-code-2024/day10.py
+++ advent-of-code-2024/day10.py
@@ -2,7 +2,7 @@ from advent import parse, digits, compose, defaultdict, grid_to_hash
 
 input = parse(10, digits)
 grid  = defaultdict(lambda: None, grid_to_hash(input))
-solve = lambda score: sum(score(dfs(head, -1, [])) for head in [ pos for pos, level in grid.items() if level == 0 ])
+solve = lambda score: sum([ score(dfs(head, -1, [])) for head, level in grid.copy().items() ])
 
 def dfs(head, prev, result):
     level = grid[head]
