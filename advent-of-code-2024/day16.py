0d43d86588bd0be01ca1e8a7c9afb9bd31924721
Brian Adkins <brian@lojic.com>
2024-12-16 16:12:33 -0500

    AoC 2024 Day 16

---
+++ advent-of-code-2024/day16.py
@@ -0,0 +1,30 @@
+from advent import parse, grid_to_hash, nx, timeit
+
+grid      = grid_to_hash(parse(16, list))
+(e,s,w,n) = dirs = (1, 1j, -1, -1j)
+start     = (1+139j, e)
+goal      = (139+1j, None)
+rotate    = { e : s, s : w, w : n, n : e }
+G         = nx.DiGraph()
+
+for pos, val in [ (pos, val) for pos, val in grid.items() if val != '#' ]:
+    for dir in dirs:
+        G.add_edge((pos, dir), (pos, rotate[dir]), weight=1000)
+        G.add_edge((pos, dir), (pos, rotate[rotate[rotate[dir]]]), weight=1000)
+
+        match grid.get(pos + dir, None):
+            case '.': G.add_edge((pos, dir), (pos+dir, dir), weight=1)
+            case 'E': G.add_edge((pos, dir), (pos+dir, None), weight=1)
+
+def part1():
+    return nx.shortest_path_length(G, source=start, target=goal, weight='weight')
+
+def part2():
+    return len({ pos
+                 for path in nx.all_shortest_paths(G, start, goal, weight='weight')
+                 for pos, _ in path })
+
+# ---------------------------------------------------------------------------------------------
+
+assert part1() == 72428
+assert part2() == 456
