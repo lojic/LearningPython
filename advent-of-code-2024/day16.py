bf888a4afb30dae0c6f329acc3dcf6b2ba6cd473
Brian Adkins <brian@lojic.com>
2024-12-16 20:07:44 -0500

    multiply by 1j or -1j to rotate

--- advent-of-code-2024/day16.py
+++ advent-of-code-2024/day16.py
@@ -4,15 +4,14 @@ grid      = grid_to_hash(parse(16, list))
 (e,s,w,n) = dirs = (1, 1j, -1, -1j)
 start     = (1+139j, e)
 goal      = (139+1j, None)
-rotate    = { e : s, s : w, w : n, n : e }
 G         = nx.DiGraph()
 
 for pos, val in [ (pos, val) for pos, val in grid.items() if val != '#' ]:
     for dir in dirs:
-        G.add_edge((pos, dir), (pos, rotate[dir]), weight=1000)                 # rotate right
-        G.add_edge((pos, dir), (pos, rotate[rotate[rotate[dir]]]), weight=1000) # rotate left
+        G.add_edge((pos, dir), (pos, dir *  1j), weight=1000) # rotate right
+        G.add_edge((pos, dir), (pos, dir * -1j), weight=1000) # rotate left
 
-        match grid.get(pos + dir, None):                                        # move forward
+        match grid.get(pos + dir, None):                      # move forward
             case '.': G.add_edge((pos, dir), (pos+dir, dir), weight=1)
             case 'E': G.add_edge((pos, dir), (pos+dir, None), weight=1)
 
