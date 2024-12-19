43fa538a7d0ab024db51f0bbecf219d990787f4c
Brian Adkins <brian@lojic.com>
2024-12-19 14:28:18 -0500

    one last readability tweak

--- advent-of-code-2024/day19_reddit.py
+++ advent-of-code-2024/day19_reddit.py
@@ -6,13 +6,17 @@ from advent import parse, words, cache, timeit
 towels, designs = parse(19, lambda s: set(words(s)), sep='\n\n')
 max_len         = max(len(pat) for pat in towels)
 
+def prefixes_suffixes(design):
+    for prefix_len in range(1, min(max_len, len(design)) + 1):
+        yield design[0:prefix_len], design[prefix_len:]
+
 @cache
 def check_design(design):
     arrangements = 1 if design in towels else 0
 
-    for prefix_len in range(1, min(max_len, len(design)) + 1):
-        if design[0:prefix_len] in towels:
-            arrangements += check_design(design[prefix_len:])
+    for prefix, suffix in prefixes_suffixes(design):
+        if prefix in towels:
+            arrangements += check_design(suffix)
 
     return arrangements
 
