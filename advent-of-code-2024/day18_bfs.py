aae42cfc400c7124e4217020527afea6469ad2ac
Brian Adkins <brian@lojic.com>
2024-12-18 21:04:55 -0500

    add manual bfs for day 18

---
+++ advent-of-code-2024/day18_bfs.py
@@ -0,0 +1,30 @@
+from advent import parse, ints, heappop, heappush
+
+blocks = { (x,y) for x, y in parse(18, ints)[:1024] }
+
+def part1(goal):
+    pq      = []
+    visited = set()
+    heappush(pq, (0, (0,0)))
+
+    while pq:
+        length, pos = heappop(pq)
+
+        if pos == goal:
+            return length
+
+        if pos in visited:
+            continue
+
+        visited.add(pos)
+
+        for dx, dy in ((1,0), (0,1), (-1,0), (0,-1)):
+            x, y = pos
+            nxt  = (nx, ny) = x + dx, y + dy
+
+            if 0 <= nx <= goal[0] and 0 <= ny <= goal[1] and nxt not in visited and nxt not in blocks:
+                heappush(pq, (length + 1, nxt))
+
+# ---------------------------------------------------------------------------------------------
+
+assert part1((70,70)) == 344
