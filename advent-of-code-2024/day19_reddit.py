d90fcd6b566c0a4d671a49c10b9938a501525495
Brian Adkins <brian@lojic.com>
2024-12-19 13:56:02 -0500

    minor refinements

--- advent-of-code-2024/day19_reddit.py
+++ advent-of-code-2024/day19_reddit.py
@@ -7,17 +7,16 @@ pat_set, designs = parse(19, lambda s: set(words(s)), sep='\n\n')
 max_pat_len      = max([len(pat) for pat in pat_set])
 
 @cache
-def check_design(design, max_len):
-    L     = min(max_len, len(design))
+def check_design(design):
     total = 1 if design in pat_set else 0
 
-    for y in range(1, L+1):
+    for y in range(1, min(max_pat_len, len(design)) + 1):
         if design[0:y] in pat_set:
-            total += check_design(design[y:], max_len)
+            total += check_design(design[y:])
 
     return total
 
-counts = [ cnt for design in designs if (cnt := check_design(design, max_pat_len)) > 0 ]
+counts = [ cnt for design in designs if (cnt := check_design(design)) > 0 ]
 
 assert len(counts) == 353             # Part 1
 assert sum(counts) == 880877787214477 # Part 2
