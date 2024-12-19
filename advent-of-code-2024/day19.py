377708d2574f7b01bb5ae61a1028b6dff6dcbbbd
Brian Adkins <brian@lojic.com>
2024-12-19 11:46:38 -0500

    use removeprefix()

--- advent-of-code-2024/day19.py
+++ advent-of-code-2024/day19.py
@@ -7,9 +7,12 @@ def check_design(design, depth=1):
     if design == '':
         return 1
     else:
-        return sum(check_design(design[len(pat):], depth + 1) for pat in pats if design.startswith(pat))
+        return sum(check_design(design.removeprefix(pat), depth + 1) for pat in pats if design.startswith(pat))
 
-counts = [ cnt for design in designs if (cnt := check_design(design)) > 0 ]
+def get_counts():    
+    counts = [ cnt for design in designs if (cnt := check_design(design)) > 0 ]
 
-assert len(counts) == 353             # Part 1
-assert sum(counts) == 880877787214477 # Part 2
+# assert len(counts) == 353             # Part 1
+# assert sum(counts) == 880877787214477 # Part 2
+
+print(timeit.timeit('get_counts()', globals=globals(), number=1))
