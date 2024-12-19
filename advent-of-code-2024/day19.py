c554988d7bfa7a552c3352743e39a89febd128e0
Brian Adkins <brian@lojic.com>
2024-12-19 14:49:44 -0500

    remove unnecessary depth param for 10x speedup

--- advent-of-code-2024/day19.py
+++ advent-of-code-2024/day19.py
@@ -1,18 +1,15 @@
-from advent import parse, words, cache, timeit
+from advent import parse, words, cache
 
 pats, designs = parse(19, words, sep='\n\n')
 
 @cache
-def check_design(design, depth=1):
+def check_design(design):
     if design == '':
         return 1
     else:
-        return sum(check_design(design.removeprefix(pat), depth + 1) for pat in pats if design.startswith(pat))
+        return sum(check_design(design.removeprefix(pat)) for pat in pats if design.startswith(pat))
 
-def get_counts():    
-    counts = [ cnt for design in designs if (cnt := check_design(design)) > 0 ]
+counts = [ cnt for design in designs if (cnt := check_design(design)) > 0 ]
 
-# assert len(counts) == 353             # Part 1
-# assert sum(counts) == 880877787214477 # Part 2
-
-print(timeit.timeit('get_counts()', globals=globals(), number=1))
+assert len(counts) == 353             # Part 1
+assert sum(counts) == 880877787214477 # Part 2
