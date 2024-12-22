6a93e41477921fad62299fad2dbfa6a381b40292
Brian Adkins <brian@lojic.com>
2024-12-22 18:57:32 -0500

    fix whitespace

--- advent-of-code-2024/day22.go
+++ advent-of-code-2024/day22.go
@@ -54,17 +54,17 @@ func getChanges(n int) [][2]int {
 }
 
 func countSequences(n int) map[[4]int]int {
-		changes := getChanges(n)
-		d       := make(map[[4]int]int)
+	changes := getChanges(n)
+	d       := make(map[[4]int]int)
 
-		for i := 3; i < len(changes); i++ {
-				t := [4]int{ changes[i-3][1], changes[i-2][1], changes[i-1][1], changes[i][1] }
-				if _, exists := d[t]; !exists {
-						d[t] = changes[i][0]
-				}
+	for i := 3; i < len(changes); i++ {
+		t := [4]int{ changes[i-3][1], changes[i-2][1], changes[i-1][1], changes[i][1] }
+		if _, exists := d[t]; !exists {
+			d[t] = changes[i][0]
 		}
+	}
 
-		return d
+	return d
 }
 
 func part1(input []int) int {
@@ -80,23 +80,23 @@ func part1(input []int) int {
 }
 
 func part2(input []int) int {
-		bananas := make(map[[4]int]int)
+	bananas := make(map[[4]int]int)
 
-		for _, n := range input {
-				for t, v := range countSequences(n) {
-						bananas[t] += v
-				}
+	for _, n := range input {
+		for t, v := range countSequences(n) {
+			bananas[t] += v
 		}
+	}
 
-		maxValue := 0
+	maxValue := 0
 
-		for _, v := range bananas {
-				if v > maxValue {
-						maxValue = v
-				}
+	for _, v := range bananas {
+		if v > maxValue {
+			maxValue = v
 		}
+	}
 
-		return maxValue
+	return maxValue
 }
 
 func main() {
