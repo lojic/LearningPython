dc2fe97f2730ebcde17c825e89067a6ff4ab4d3c
Brian Adkins <brian@lojic.com>
2024-12-23 11:13:23 -0500

    don't need n

--- advent-of-code-2024/day23.py
+++ advent-of-code-2024/day23.py
@@ -3,7 +3,7 @@ from advent import parse, words, nx, combinations
 G = nx.Graph(parse(23, words))
 
 part1 = lambda: len({ tuple(sorted((v, neighbors[0], neighbors[1])))
-                      for v, n in nx.triangles(G).items() if v[0] == 't'
+                      for v, _ in nx.triangles(G).items() if v[0] == 't'
                       for neighbors in combinations(G[v], 2) if G.has_edge(neighbors[0], neighbors[1]) })
 
 part2 = lambda: ",".join(sorted(nx.max_weight_clique(G, weight=None)[0]))
