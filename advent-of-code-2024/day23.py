6ea1eab9bd8bd12a1c00115c6145e91fc970ab56
Brian Adkins <brian@lojic.com>
2024-12-23 11:08:51 -0500

    AoC 2024 Day 23

---
+++ advent-of-code-2024/day23.py
@@ -0,0 +1,14 @@
+from advent import parse, words, nx, combinations
+
+G = nx.Graph(parse(23, words))
+
+part1 = lambda: len({ tuple(sorted((v, neighbors[0], neighbors[1])))
+                      for v, n in nx.triangles(G).items() if v[0] == 't'
+                      for neighbors in combinations(G[v], 2) if G.has_edge(neighbors[0], neighbors[1]) })
+
+part2 = lambda: ",".join(sorted(nx.max_weight_clique(G, weight=None)[0]))
+
+# ---------------------------------------------------------------------------------------------
+
+assert part1() == 1151
+assert part2() == 'ar,cd,hl,iw,jm,ku,qo,rz,vo,xe,xm,xv,ys'
