8bc1d4220c28aa75139a94b26925437b2539d1b1
Brian Adkins <brian@lojic.com>
2024-12-22 19:50:19 -0500

    add parallel version for day22

---
+++ advent-of-code-2024/day22_parallel.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+	"strconv"
+	"sync"
+)
+
+func parse(filename string) []int {
+	file, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	var numbers []int
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		num, err := strconv.Atoi(line)
+		if err != nil {
+			panic(err)
+		}
+		numbers = append(numbers, num)
+	}
+
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+
+	return numbers
+}
+
+func evolve(n int) int {
+	n = (n*64 ^ n) % 16777216
+	n = (n/32 ^ n) % 16777216
+	return (n*2048 ^ n) % 16777216
+}
+
+func getChanges(n int) [][2]int {
+	prevDigit := n % 10
+	changes   := make([][2]int, 0, 2000)
+
+	for i := 0; i < 2000; i++ {
+		n         = evolve(n)
+		digit    := n % 10
+		changes   = append(changes, [2]int{digit, digit - prevDigit})
+		prevDigit = digit
+	}
+
+	return changes
+}
+
+func countSequences(n int) map[[4]int]int {
+	changes := getChanges(n)
+	d       := make(map[[4]int]int)
+
+	for i := 3; i < len(changes); i++ {
+		t := [4]int{ changes[i-3][1], changes[i-2][1], changes[i-1][1], changes[i][1] }
+		if _, exists := d[t]; !exists {
+			d[t] = changes[i][0]
+		}
+	}
+
+	return d
+}
+
+func part1(input []int) int {
+	totalSum := 0
+
+	for _, n := range input {
+		for i := 0; i < 2000; i++ {
+			n = evolve(n)
+		}
+		totalSum += n
+	}
+	return totalSum
+}
+
+func part2(input []int) int {
+	var bananas sync.Map
+
+	var wg sync.WaitGroup
+	for _, n := range input {
+		wg.Add(1)
+		go func(n int) {
+			defer wg.Done()
+			for t, v := range countSequences(n) {
+				actual, _ := bananas.LoadOrStore(t, 0)
+				bananas.Store(t, actual.(int)+v)
+			}
+		}(n)
+	}
+
+	wg.Wait()
+
+	maxValue := 0
+	bananas.Range(func(_, value any) bool {
+		if value.(int) > maxValue {
+			maxValue = value.(int)
+		}
+		return true
+	})
+
+	return maxValue
+}
+
+func main() {
+	if part2(parse("day22.txt")) != 2189 {
+		log.Fatalf("Part 2 failed")
+	}
+}
