b7e445b6e5da99d40cbcf4acdf52eb274aae9f65
Brian Adkins <brian@lojic.com>
2024-12-23 12:35:33 -0500

    white space

--- advent-of-code-2024/day23_visualize.py
+++ advent-of-code-2024/day23_visualize.py
@@ -1,10 +1,8 @@
 from advent import nx, plt
 
-G = nx.Graph([ ('A','B', { 'weight' : 5 }), ('A','C', { 'weight' : 50 }), ('A','D', { 'weight' : 25 }), ('A','E', { 'weight' : 25 }),
-               ('B','D', { 'weight' : 20 }), ('B','E', { 'weight' : 5 }),
-               ('C','F', { 'weight' : 55 }),
-               ('D','E', { 'weight' : 25 }),
-               ('E','F', { 'weight' : 50 }) ])
+G = nx.Graph([ ('A','B', { 'weight' : 5 }), ('A','C', { 'weight' : 50 }), ('A','D', { 'weight' : 25 }),
+               ('A','E', { 'weight' : 25 }), ('B','D', { 'weight' : 20 }), ('B','E', { 'weight' : 5 }),
+               ('C','F', { 'weight' : 55 }), ('D','E', { 'weight' : 25 }), ('E','F', { 'weight' : 50 }) ])
 
 pos = { 'A' : (-1, -5), 'B' : (0,10), 'C' : (-1,-10), 'D' : (0,0), 'E' : (1,-5), 'F': (1,-10) }
 plt.figure(figsize=(8, 8))
