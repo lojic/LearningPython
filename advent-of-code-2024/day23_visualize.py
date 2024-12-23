63e97e2651678288caf4f5e191ce82a3bfd4845b
Brian Adkins <brian@lojic.com>
2024-12-23 12:54:01 -0500

    highlight clique edges

--- advent-of-code-2024/day23_visualize.py
+++ advent-of-code-2024/day23_visualize.py
@@ -4,8 +4,11 @@ G = nx.Graph([ ('A','B'), ('A','C'), ('A','D'),
                ('A','E'), ('B','D'), ('B','E'),
                ('C','F'), ('D','E'), ('E','F') ])
 
+clique_edges = [ ('A','B'), ('A','D'), ('A','E'), ('B','D'), ('B','E'), ('D','E') ]
+
 pos = { 'A' : (-1, -5), 'B' : (0, 10), 'C' : (-1, -10),
         'D' : ( 0,  0), 'E' : (1, -5), 'F':  ( 1, -10) }
 plt.figure(figsize=(8, 8))
 nx.draw(G, pos, with_labels=True, node_color="lightblue", node_size=5000, font_size=30)
+nx.draw_networkx_edges(G, pos, edgelist=clique_edges, edge_color="green", width=4)
 plt.show()
