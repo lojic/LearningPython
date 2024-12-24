559e91329018ac1d56032f5defe0ec794a8f8601
Brian Adkins <brian@lojic.com>
2024-12-23 20:34:47 -0500

    AoC 2024 Day 20

--- advent-of-code-2024/day20.py
+++ advent-of-code-2024/day20.py
@@ -1,59 +1,34 @@
 from advent import parse, grid_to_hash, nx
 
-lines       = parse(20, list)
-width       = len(lines[0])
-height      = len(lines)
-grid        = grid_to_hash(lines)
-start       = [ pos for pos, c in grid.items() if c == 'S' ][0]
-end         = [ pos for pos, c in grid.items() if c == 'E' ][0]
-grid[start] = grid[end] = '.'
-candidates  = []
+def gen_edges(grid):
+    for pos, c in grid.items():
+        if c in '.SE':
+            if grid.get(pos + 1, None) in '.SE':
+                yield (pos, pos + 1)
+            if grid.get(pos + 1j, None)in '.SE':
+                yield (pos, pos + 1j)
 
-def gen_edges():
-    for x in range(width):
-        for y in range(height):
-            pos     = complex(x, y)
-            pos_c   = grid[pos]
-            east    = pos + 1
-            east_c  = grid.get(east,  None)
-            south   = pos + 1j
-            south_c = grid.get(south, None)
+def solve():
+    grid        = grid_to_hash(parse(20, list))
+    start       = [ pos for pos, c in grid.items() if c == 'S' ][0]
+    goal        = [ pos for pos, c in grid.items() if c == 'E' ][0]
+    G           = nx.Graph(gen_edges(grid))
+    path_d      = { pos : i for i, pos in enumerate(nx.shortest_path(G, start, goal)) }
+    min_savings = 100
+    part1       = 0
+    part2       = 0
 
-            if pos_c == '.':
-                if east_c == '.':
-                    yield (pos, east)
-                if south_c == '.':
-                    yield (pos, south)
-            elif pos_c == '#':
-                west_c  = grid.get(pos - 1,  None)
-                north_c = grid.get(pos - 1j, None)
+    for pos, i in path_d.items():
+        for dx in range(-20, 20 + 1):
+            for dy in range(-(20 - abs(dx)), (20 - abs(dx)) + 1):
+                pos2 = pos + (dx + dy * 1j)
+                if pos2 in path_d:
+                    dist = abs(dx) + abs(dy)
+                    if (path_d[pos2] - i) - dist >= min_savings:
+                        part2 += 1
+                        part1 += 1 if dist <= 2 else 0
+    return part1, part2
 
-                if (east_c == '.' and west_c == '.') or (north_c == '.' and south_c == '.'):
-                    candidates.append(pos)
+# ---------------------------------------------------------------------------------------------
 
-def remove_wall(g, pos):
-    if grid.get(pos - 1, None) == '.' and grid.get(pos + 1, None) == '.':
-        g.add_edge(pos - 1, pos)
-        g.add_edge(pos, pos + 1)
-
-    if grid.get(pos - 1j, None) == '.' and grid.get(pos + 1j, None) == '.':
-        g.add_edge(pos - 1j, pos)
-        g.add_edge(pos, pos + 1j)
-
-def replace_wall(g, pos):
-    g.remove_node(pos)
-
-def part1():
-    G = nx.Graph()
-    G.add_edges_from(gen_edges())
-    path_length = nx.shortest_path_length(G, start, end)
-
-    def cheat_ok(pos):
-        remove_wall(G, pos)
-        cheat_length = nx.shortest_path_length(G, start, end)
-        replace_wall(G, pos)
-        return path_length - cheat_length >= 100
-
-    return sum(1 for pos in candidates if cheat_ok(pos))
-
-assert part1() == 1454
+assert solve() == (1454, 997879) # 2.2 seconds
