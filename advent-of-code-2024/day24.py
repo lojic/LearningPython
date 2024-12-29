e328d25c3448f41fdf9dc3cb5039b81009682f01
Brian Adkins <brian@lojic.com>
2024-12-29 17:25:29 -0500

    AoC 2024 Day 24 with help for part 2

--- advent-of-code-2024/day24.py
+++ advent-of-code-2024/day24.py
@@ -1,16 +1,16 @@
+# Part 1 on my own. Part 2 via ideas from lscddit on reddit.
 from advent import parse, atoms
 
 wires, exprs = parse(24, lambda s: [ atoms(line) for line in str.split(s,'\n') ], sep='\n\n')
+d = {}
+
+for wire, value in wires:
+    d[wire] = value
+
+for (w1, op, w2, _, out) in exprs:
+    d[out] = (op, w1, w2)
 
 def part1():
-    d = {}
-
-    for wire, value in wires:
-        d[wire] = value
-
-    for (w1, op, w2, _, out) in exprs:
-        d[out] = (op, w1, w2)
-
     def evaluate(expr):
         match expr:
             case int(value): return value
@@ -25,6 +25,33 @@ def part1():
 
     return sum((evaluate(key) << i) for i, key in enumerate(sorted([ key for key in d if key[0] == 'z' ])))
 
+def part2():
+    operations = [ (key, val) for key, val in d.items() if isinstance(val, tuple) ]
+    wrong      = set()
+
+    for out, (op, w1, w2) in operations:
+        if out[0] == 'z' and op != 'XOR' and out != 'z45':
+            wrong.add(out)
+
+        if op == 'XOR' and \
+           out[0] not in ('x', 'y', 'z') and \
+           w1[0] not in ('x', 'y', 'z') and \
+           w2[0] not in ('x', 'y', 'z'):
+            wrong.add(out)
+
+        if op == 'AND' and 'x00' not in (w1, w2):
+            for out2, (op2, w1_2, w2_2) in operations:
+                if (out == w1_2 or out == w2_2) and op2 != 'OR':
+                    wrong.add(out)
+
+        if op == 'XOR':
+            for out2, (op2, w1_2, w2_2) in operations:
+                if (out == w1_2 or out == w2_2) and op2 == 'OR':
+                    wrong.add(out)
+
+    return ",".join(sorted(wrong))
+
 # ---------------------------------------------------------------------------------------------
 
 assert part1() == 56729630917616
+assert part2() == 'bjm,hsw,nvr,skf,wkr,z07,z13,z18'
