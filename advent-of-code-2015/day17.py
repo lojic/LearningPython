4e163d881db4b0a35fecf1b2bcc02d23b4af2b61
Brian Adkins <brian@lojic.com>
2025-01-04 10:11:47 -0500

    simplify with lambda

--- advent-of-code-2015/day17.py
+++ advent-of-code-2015/day17.py
@@ -2,11 +2,10 @@ from advent import parse, combinations
 
 containers = parse(17, int)
 
-def solve(fun):
-    return fun(ways
-               for n in range(1, len(containers) + 1)
-               if (ways := sum(1 for combo in combinations(containers, n)
-                               if sum(combo) == 150)))
+solve = lambda fun: fun(ways
+                        for n in range(1, len(containers) + 1)
+                        if (ways := sum(1 for combo in combinations(containers, n)
+                                        if sum(combo) == 150)))
 
 # ---------------------------------------------------------------------------------------------
 
