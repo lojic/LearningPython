509a19193e32cb8cf334cdf8ac6f91069cb88f99
Brian Adkins <brian@lojic.com>
2025-01-06 12:31:29 -0500

    Hanukkah of Data 5784 - Day 4

---
+++ hanukkah-of-data-5784/day4.py
@@ -0,0 +1,27 @@
+from advent import Counter
+import pandas as pd
+
+def solve():
+    order_items   = pd.read_csv('noahs-orders_items.csv')
+    orders        = pd.read_csv('noahs-orders.csv')
+    customers     = pd.read_csv('noahs-customers.csv')
+    bakery_items  = order_items[order_items['sku'].str.startswith('BKY')]['orderid']
+    bakery_orders = pd.merge(orders, bakery_items)[['orderid','customerid','ordered','shipped']].sort_values('shipped')
+
+    prev_date      = None
+    first_customer = Counter()
+
+    for index, row in bakery_orders.iterrows():
+        shipped_date = str.split(row['shipped'])[0]
+
+        if shipped_date != prev_date:
+            first_customer[row['customerid']] += 1
+            prev_date = shipped_date
+
+    frequent_first_customer_id = first_customer.most_common()[0][0]
+
+    return customers[customers['customerid'] == frequent_first_customer_id].iloc[0]['phone']
+
+# ---------------------------------------------------------------------------------------------
+
+assert solve() == '607-231-3605'
