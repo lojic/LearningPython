1546c57077d2547fff83f323dfd639143ab603f4
Brian Adkins <brian@lojic.com>
2025-01-06 12:44:54 -0500

    reorder statements for clarity

--- hanukkah-of-data-5784/day4.py
+++ hanukkah-of-data-5784/day4.py
@@ -3,9 +3,9 @@ import pandas as pd
 
 def solve():
     order_items   = pd.read_csv('noahs-orders_items.csv')
-    orders        = pd.read_csv('noahs-orders.csv')
-    customers     = pd.read_csv('noahs-customers.csv')
     bakery_items  = order_items[order_items['sku'].str.startswith('BKY')]['orderid']
+
+    orders        = pd.read_csv('noahs-orders.csv')
     bakery_orders = pd.merge(orders, bakery_items)[['orderid','customerid','ordered','shipped']].sort_values('shipped')
 
     prev_date      = None
@@ -19,6 +19,7 @@ def solve():
             prev_date = shipped_date
 
     frequent_first_customer_id = first_customer.most_common()[0][0]
+    customers                  = pd.read_csv('noahs-customers.csv')
 
     return customers[customers['customerid'] == frequent_first_customer_id].iloc[0]['phone']
 
