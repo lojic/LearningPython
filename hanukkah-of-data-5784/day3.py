fb42017c957ddffb23d5a26d149aba227df862eb
Brian Adkins <brian@lojic.com>
2025-01-07 17:47:19 -0500

    wrap with solve()

--- hanukkah-of-data-5784/day3.py
+++ hanukkah-of-data-5784/day3.py
@@ -1,20 +1,20 @@
 import pandas as pd
 
-rabbit_years = (1939, 1951, 1963, 1975, 1987, 1999, 2011, 2023)
-
-customers          = pd.read_csv('noahs-customers.csv', parse_dates=['birthdate'])
-contractor_address = customers[customers['phone'] == '332-274-4185'].iloc[0]['citystatezip']
-
-bd          = customers['birthdate']
-birth_month = bd.dt.month
-birth_day   = bd.dt.day
-birth_year  = bd.dt.year
-
-is_cancer       = ((birth_month == 6) & (birth_day >= 21)) | ((birth_month == 7) & (birth_day <= 22))
-is_rabbit_year  = birth_year.isin(rabbit_years)
-is_neighborhood = customers['citystatezip'] == contractor_address
-
 def solve():
+    rabbit_years = (1939, 1951, 1963, 1975, 1987, 1999, 2011, 2023)
+
+    customers          = pd.read_csv('noahs-customers.csv', parse_dates=['birthdate'])
+    contractor_address = customers[customers['phone'] == '332-274-4185'].iloc[0]['citystatezip']
+
+    bd          = customers['birthdate']
+    birth_month = bd.dt.month
+    birth_day   = bd.dt.day
+    birth_year  = bd.dt.year
+
+    is_cancer       = ((birth_month == 6) & (birth_day >= 21)) | ((birth_month == 7) & (birth_day <= 22))
+    is_rabbit_year  = birth_year.isin(rabbit_years)
+    is_neighborhood = customers['citystatezip'] == contractor_address
+
     return customers[is_cancer & is_rabbit_year & is_neighborhood].iloc[0]['phone']
 
 # -------------------------------------------------------------------------------------
