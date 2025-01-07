e1364e17ce4f46d2a70b87fc91f14701e6eeaa19
Brian Adkins <brian@lojic.com>
2025-01-06 20:39:19 -0500

    Use pandas more directly

--- hanukkah-of-data-5784/day6.py
+++ hanukkah-of-data-5784/day6.py
@@ -5,12 +5,9 @@ order_items = pd.merge(pd.read_csv('noahs-orders.csv'),
                        pd.merge(pd.read_csv('noahs-orders_items.csv'),
                                 pd.read_csv('noahs-products.csv')))
 
-sale_items = order_items[order_items['unit_price'] < order_items['wholesale_cost']]
-customers = pd.read_csv('noahs-customers.csv')
+sale_items  = order_items[order_items['unit_price'] < order_items['wholesale_cost']]
+grouped     = sale_items[['customerid','unit_price']].groupby('customerid').count()
+customer_id = grouped[grouped['unit_price'] == grouped['unit_price'].max()].reset_index().iloc[0]['customerid']
+customers   = pd.read_csv('noahs-customers.csv')
 
-c = Counter()
-
-for index, row in sale_items.iterrows():
-    c[row['customerid']] += 1
-
-assert customers[customers['customerid'] == c.most_common()[0][0]].iloc[0]['phone'] == '585-838-9161'
+assert customers[customers['customerid'] == customer_id].iloc[0]['phone'] == '585-838-9161'
