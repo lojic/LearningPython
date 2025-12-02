676d812d17438449be82baafe1dd0acaee140732
Brian Adkins <brian@lojic.com>
2025-12-02 11:57:10 -0500

    AoC 2025 Day 2

---
+++ advent-of-code-2025/app/day02.py
@@ -0,0 +1,26 @@
+from advent import parse, positive_ints, Callable
+
+input = parse(2, positive_ints, sep=',')
+
+
+part1 = lambda s: s[: len(s) // 2] * 2 == s
+
+
+def part2(s: str) -> bool:
+    length = len(s)
+
+    for i in range(1, (length // 2) + 1):
+        quotient, remainder = divmod(length, i)
+
+        if remainder == 0 and s[:i] * quotient == s:
+            return True
+
+    return False
+
+
+def solve(part: Callable[[str], bool]):
+    return sum(sum(n for n in range(left, right + 1) if part(str(n))) for left, right in input)
+
+
+assert solve(part1) == 23560874270
+assert solve(part2) == 44143124633
