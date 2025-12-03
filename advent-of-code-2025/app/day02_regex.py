d6e361c5262be09726fefa796f08bcc33a51a11a
Brian Adkins <brian@lojic.com>
2025-12-02 20:02:31 -0500

    Add regex day 2

---
+++ advent-of-code-2025/app/day02_regex.py
@@ -0,0 +1,14 @@
+from advent import parse, positive_ints, Callable, re
+
+input = parse(2, positive_ints, sep=',')
+
+part1 = lambda s: re.match(r'^(\d+)\1$', s)
+part2 = lambda s: re.match(r'^(\d+)\1+$', s)
+
+
+def solve(invalid: Callable[[str], bool]):
+    return sum(sum(n for n in range(left, right + 1) if invalid(str(n))) for left, right in input)
+
+
+assert solve(part1) == 23560874270
+assert solve(part2) == 44143124633
