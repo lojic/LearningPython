3e567773aa5054a3b8cc3f7efa1e5874a58001d8
Brian Adkins <brian@lojic.com>
2025-12-04 09:53:37 -0500

    shorten to accessible

--- advent-of-code-2025/app/day04.py
+++ advent-of-code-2025/app/day04.py
@@ -3,11 +3,11 @@ from advent import parse, grid_to_hash, Generator
 grid: dict[complex, str] = grid_to_hash(parse(4, list), elem_filter=lambda c: c == '@')
 dirs: tuple[complex, ...] = (-1j, 1 - 1j, 1, 1 + 1j, 1j, -1 + 1j, -1, -1 - 1j)
 adjacent = lambda pos: [neighbor for dir in dirs if grid.get(neighbor := pos + dir)]
-accessible_rolls = lambda grid: [pos for pos in grid.keys() if len(adjacent(pos)) < 4]
+accessible = lambda grid: [pos for pos in grid.keys() if len(adjacent(pos)) < 4]
 
 
 def remove_rolls(grid: dict[complex, str]) -> Generator[int, None, None]:
-    while rolls := accessible_rolls(grid):
+    while rolls := accessible(grid):
         for pos in rolls:
             del grid[pos]
         yield len(rolls)
