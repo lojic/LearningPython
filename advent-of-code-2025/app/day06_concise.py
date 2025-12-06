94453aed2bfafba4c486ca68076541dd33e60965
Brian Adkins <brian@lojic.com>
2025-12-06 11:54:26 -0500

    more concise

--- advent-of-code-2025/app/day06_concise.py
+++ advent-of-code-2025/app/day06_concise.py
@@ -5,20 +5,16 @@ apply = lambda op_sym, args: reduce({'+': operator.add, '*': operator.mul}[op_sy
 part1 = lambda: sum([apply(col[-1], col[:-1]) for col in zip(*parse(6, atoms))])
 
 
-def part2():
-    def answers(args=[]):
-        for row in filter(
-            lambda row: ''.join(row).strip(),
-            zip(*parse(6, compose(reversed, list), do_rstrip=False)),
-        ):
-            args.append(int(''.join(row[:-1]).strip()))
-
-            if row[-1] in ['+', '*']:
-                yield apply(row[-1], args)
-                args = []
-
-    return sum(answers())
+def part2(args=[]):
+    for row in filter(
+        lambda row: ''.join(row).strip(),
+        zip(*parse(6, compose(reversed, list), do_rstrip=False)),
+    ):
+        args.append(int(''.join(row[:-1]).strip()))
+        if row[-1] in ['+', '*']:
+            yield apply(row[-1], args)
+            args = []
 
 
 assert part1() == 3525371263915
-assert part2() == 6846480843636
+assert sum(part2()) == 6846480843636
