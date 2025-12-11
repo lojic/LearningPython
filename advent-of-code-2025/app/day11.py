4f500409f9e6196caeaa8bfaca3d8dabf1caa2dc
Brian Adkins <brian@lojic.com>
2025-12-11 11:38:08 -0500

    AoC 2025 Day 11

---
+++ advent-of-code-2025/app/day11.py
@@ -0,0 +1,33 @@
+"""Advent of Code 2025: Day 11 - Reactor"""
+
+from advent import parse, atoms, nx, prod
+
+G = nx.DiGraph()
+
+for source, *sinks in parse(11, atoms, print_lines=3):
+    for sink in sinks:
+        G.add_edge(source, sink)
+
+
+def part1():
+    return len(list(nx.all_simple_paths(G, 'you', 'out')))
+
+
+def part2():
+    def dfs(node, sink, seen):
+        if node in seen:
+            return seen[node]
+        elif node == sink:
+            return 1
+        else:
+            seen[node] = sum(dfs(n, sink, seen) for n in G.neighbors(node))
+            return seen[node]
+
+    return sum(
+        prod(dfs(src, dst, {}) for src, dst in zip(seq, seq[1:]))
+        for seq in (('svr', 'fft', 'dac', 'out'), ('svr', 'dac', 'fft', 'out'))
+    )
+
+
+assert part1() == 640
+assert part2() == 367579641755680
