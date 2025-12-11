38e9e7c0f73873e74dfb73b6cca2c44d240c331c
Brian Adkins <brian@lojic.com>
2025-12-11 11:46:40 -0500

    use dfs for both parts

--- advent-of-code-2025/app/day11.py
+++ advent-of-code-2025/app/day11.py
@@ -9,15 +9,16 @@ for source, *sinks in parse(11, atoms, print_lines=3):
         G.add_edge(source, sink)
 
 
+@cache
+def dfs(node, sink):
+    return 1 if node == sink else sum(dfs(n, sink) for n in G.neighbors(node))
+
+
 def part1():
-    return len(list(nx.all_simple_paths(G, 'you', 'out')))
+    return dfs('you', 'out')
 
 
 def part2():
-    @cache
-    def dfs(node, sink):
-        return 1 if node == sink else sum(dfs(n, sink) for n in G.neighbors(node))
-
     return sum(
         prod(dfs(src, dst) for src, dst in zip(seq, seq[1:]))
         for seq in (('svr', 'fft', 'dac', 'out'), ('svr', 'dac', 'fft', 'out'))
